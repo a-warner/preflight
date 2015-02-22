080ad76c40b4d07ba0be9943db0bcfbd515a9c42
Andrew Warner <wwarner.andrew@gmail.com>
2015-02-21 20:09:10 -0500

    Test webhook signature

--- app/workers/github_webhook_worker.rb
+++ app/workers/github_webhook_worker.rb
@@ -1,5 +1,7 @@
 class GithubWebhookWorker < Struct.new(:headers, :body)
-  WANTED_HEADERS = %w(X-GitHub-Event X-GitHub-Delivery X-Hub-Signature)
+  HUB_SIGNATURE = 'X-Hub-Signature'
+  WANTED_HEADERS = %w(X-GitHub-Event X-GitHub-Delivery) << HUB_SIGNATURE
+  InvalidSignature = Class.new(StandardError)
 
   def self.get_headers(headers)
     WANTED_HEADERS.each_with_object({}) do |header, hash|
@@ -8,5 +10,13 @@ class GithubWebhookWorker < Struct.new(:headers, :body)
   end
 
   def perform
+    verify_signature!
+  end
+
+  def verify_signature!
+    hub_signature = headers[HUB_SIGNATURE].to_s.split('=', 2).last
+    signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV.fetch('GITHUB_WEBHOOK_SECRET'), body)
+
+    raise InvalidSignature unless hub_signature == signature
   end
 end
