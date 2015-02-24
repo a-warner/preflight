d01178083ab65ca908d4baee8c7f13885d8ed7b0
Andrew Warner <wwarner.andrew@gmail.com>
2015-02-23 22:29:46 -0500

    Move form outside of table
    - Otherwise rails won't handle data-remote in the case of turbolinks
      navigation

--- app/assets/javascripts/checklists.js
+++ app/assets/javascripts/checklists.js
@@ -1,13 +1,11 @@
-$(function() {
-  $(document).on('ajax:success', '.destroy-checklist-item', function() {
-    $(this).closest('tr').remove()
-  })
+$(document).on('ajax:success', '.destroy-checklist-item', function() {
+  $(this).closest('tr').remove()
+})
 
-  $(document).on('ajax:success', '.new-checklist', function(e, data) {
-    var $form = $(this)
-    $form[0].reset()
-    $(data).insertBefore($form.closest('tr'))
-  }).on('ajax:error', '.new-checklist', function(e, xhr) {
-    alert(xhr.responseText)
-  })
+$(document).on('ajax:success', '.new-checklist', function(e, data) {
+  var $form = $(this)
+  $form[0].reset()
+  $(data).insertBefore($form.find('.new'))
+}).on('ajax:error', '.new-checklist', function(e, xhr) {
+  alert(xhr.responseText)
 })
