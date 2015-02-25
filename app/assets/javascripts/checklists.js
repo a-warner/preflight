ba0a89a8cf204040f5eaf70742ee9f9eaf585e28
Andrew Warner <wwarner.andrew@gmail.com>
2015-02-24 23:37:49 -0500

    Fix selector

--- app/assets/javascripts/checklists.js
+++ app/assets/javascripts/checklists.js
@@ -6,7 +6,7 @@ $(document).on('ajax:success', '.new_checklist_item', function(e, data) {
   var $form = $(this)
   $form[0].reset()
   $(data).insertBefore($form.closest('.row'))
-}).on('ajax:error', '.new-checklist, .edit_checklist_item', function(e, xhr) {
+}).on('ajax:error', '.new_checklist_item, .edit_checklist_item', function(e, xhr) {
   alert(xhr.responseText)
 })
 
