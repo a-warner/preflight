a5144f1b2b87a42b40783d2f00e0472b8b43f5fe
Andrew Warner <wwarner.andrew@gmail.com>
2015-03-03 00:02:06 -0500

    Wrap all app javascripts in an anonymous function

---
+++ config/initializers/sprockets.rb
@@ -0,0 +1,7 @@
+Rails.application.assets.register_postprocessor 'application/javascript', :close_over_app_javascripts do |context, data|
+  if context.pathname.to_s.starts_with?(Rails.root.join('app/assets/javascripts').to_s)
+    "(function () {\n'use strict';\n#{data}\n}).call(this);"
+  else
+    data
+  end
+end
