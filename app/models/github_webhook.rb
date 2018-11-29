eef88f5741ad4e270b2aa89375773caede31ebb9
Andrew Warner <wwarner.andrew@gmail.com>
2018-11-29 09:23:13 -0500

    Remove now-unused `hook!` method

--- app/models/github_webhook.rb
+++ app/models/github_webhook.rb
@@ -5,15 +5,6 @@ class GithubWebhook < ActiveRecord::Base
   validates :created_by, presence: true
   validates :github_id, :github_repository, presence: true, uniqueness: true
 
-  def self.hook!(created_by, repository)
-    where(github_repository_id: repository.id).first_or_create! do |r|
-      webhook = created_by.github_client.create_default_hook(repository)
-
-      r.github_id = webhook.id
-      r.created_by = created_by
-    end
-  end
-
   def self.unhook_all_hooked_by_old_oauth_integration!
     where(hook_deleted_at: nil).find_each do |hook|
       hook.transaction do
